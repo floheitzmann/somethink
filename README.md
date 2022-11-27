fedaaabcce911181cac9ad8c74fa66d78b66578c
Flo <heitzmnn@gmail.com>
2022-11-27 19:09:11 +0100

    First Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# somethink
+
+A mobile game.
---
+++ android/app/src/main/kotlin/dev/floheitzmann/somethink/MainActivity.kt
@@ -0,0 +1,6 @@
+package dev.floheitzmann.somethink
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
